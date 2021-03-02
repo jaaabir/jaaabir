c789ed0ee88542e55ba11018a7396928c6643b27
jaabir <jaabir.zaid@gmail.com>
2021-03-02 15:42:06 +0530

    Bio

---
+++ text_animation.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  var dataText = [
+    "Name : jaabir ",
+    "What i am good at ? ",
+    "Python | React js | Django",
+    "HTML | CSS | js ",
+  ];
+  function typeWriter(text, i, fnCallback) {
+    if (i < text.length) {
+      document.querySelector("h3").innerHTML =
+        text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback);
+      }, 150);
+    } else if (typeof fnCallback == "function") {
+      setTimeout(fnCallback, 700);
+    }
+  }
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == "undefined") {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 20000);
+    }
+    if (i < dataText[i].length) {
+      typeWriter(dataText[i], 0, function () {
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  StartTextAnimation(0);
+});
